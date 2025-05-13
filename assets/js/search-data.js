9077e2e689c6083c11ed0a8fb80a44e97721f48d
ticmis <42403786+ticmis@users.noreply.github.com>
2025-05-13 11:49:03 +0000

    Deploying to gh-pages from @ ticmis/ticmis.github.io@20dd82f3d191d656faf636ddccb5e55f119dd963 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -419,16 +419,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-our-group-website-is-released",
+          title: 'Our group website is released!',
           description: "",
           section: "News",},{id: "projects-spin-orbit-torque",
           title: 'Spin-orbit Torque',
